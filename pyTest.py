047d884c0a0650503d1f9f0d815cc576facb9d9e
qingsen <903241387@qq.com>
2016-12-13 00:00:19 +0800

    1

--- pyTest.py
+++ pyTest.py
@@ -4,6 +4,7 @@
 #第一行注释是为了告诉Linux/OS X系统，这是一个Python可执行程序，Windows系统会忽略这个注释；
 #第二行注释是为了告诉Python解释器(运行python就是启动CPython解释器 解释型语言,运行速度慢,不能加密)，按照UTF-8编码读取源代码，否则，你在源代码中写的中文输出可能会有乱码。
 
+# cmd C:\Python27\Scripts 执行pip install packname 或 easy_install packname
 import functools
 import sys   #import sys as "别名"
 import os
